a73e5ab536021b6bf2cd3edd4ddd5959a49e868c
gthneo <gthneo@gmail.com>
2017-06-11 08:10:53 +0800

    myfirst.cpp
    myfirst C++

---
+++ myfirst.cpp
@@ -0,0 +1,11 @@
+//my first.cpp --displays a message               
+#include <iostream>                           //a PREPROCESSOR directive 
+int main()                                    //funtion header
+{                                             //start of function body
+  using namespace std;                        //make definitions visible
+  cout << "Come up and C++ me somre time.";   //message
+  count << endl;                              //start a new line
+  count << "you won't regret it!" << endl;    //more output
+  return 0;                                   //terminame main{}
+  }                                           //end of function body
+ 
